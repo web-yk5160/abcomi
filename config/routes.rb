18df92a1d1ebab410074e7c0dcce87d34242ac2a
yuta kimura <rubystudy0123@gmail.com>
2021-05-16 17:06:58 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/index'
+  get 'pages/show'
+end
---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Pages", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/pages/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/pages/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
