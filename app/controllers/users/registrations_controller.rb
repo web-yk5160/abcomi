248187aff5219ccf87abc78bdbcd0bd81601e12b
yuta kimura <rubystudy0123@gmail.com>
2021-05-29 18:32:36 +0900

    feat: ゲストログイン機能 #4

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,11 +2,18 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
+  before_action :ensure_normal_user, only: %i[update destroy]
 
   def new
     @user = User.new
     @study_abroad_expectation = @user.study_abroad_expectations.build
     @study_abroad_target = @user.study_abroad_targets.build
+  end
+
+  def ensure_normal_user
+    if resource.email == "guest@example.com"
+      redirect_to root_path, alert: "ゲストユーザーの更新・削除はできません。"
+    end
   end
 
   protected
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,10 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
+
+  def self.guest 
+    find_or_create_by!(email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
