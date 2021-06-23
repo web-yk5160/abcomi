45eba33c6bac06cd53f435d9ee211f135c41080b
yuta kimura <rubystudy0123@gmail.com>
2021-06-23 10:38:57 +0900

    feat: 新規投稿ページ等のデザイン修正 #8

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,6 +10,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @study_abroad_target = @user.study_abroad_targets.build
   end
 
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+ 
+    #if update_resource(resource, account_update_params)
+    if resource.update_without_current_password(account_update_params)
+      yield resource if block_given?
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
   def ensure_normal_user
     if resource.email == "guest@example.com"
       redirect_to root_path, alert: "ゲストユーザーの更新・削除はできません。"
@@ -28,4 +48,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
                                               { study_abroad_expectations_attributes: [:expected_country, :expected_state, :id],
                                                 study_abroad_targets_attributes: [:target_country, :target_state, :id] }])
   end
+
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
+  end
+
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -50,4 +50,15 @@ class User < ApplicationRecord
       user.password = SecureRandom.urlsafe_base64
     end
   end
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
 end
